a1c93e718105586693c6510e162cbb8477ccbdaf
jjGabriel <songy8@mcmaster.ca>
2017-06-11 18:32:20 -0700

    add database

--- firstWeb.py
+++ firstWeb.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, session, redirect, url_for
-from flask_script import Manager
+import os
+from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import Form
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from datetime import datetime 
@@ -15,22 +19,73 @@ moment = Moment(app)
 #configure an encryption key for CSRF protection
 app.config['SECRET_KEY'] = 'csrfkey'
 
+#Database configuration
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+manager.add_command('db', MigrateCommand)
+mail = Mail(app)
+
+#configure send email through gmail account
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USER_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	users = db.relationship('User', backref='role')
+
+	def __repr__(self):
+		return '<Role %r>' % self.name
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	def __repr__(self):
+		return '<User %r>' % self.username
 
 class NameForm(Form):
 	name = StringField('What is your name?', validators=[Required()])
 	submit = SubmitField('Submit')
+
+#add objects to import list in shell command
+def make_shell_context():
+	return dict(app=app, db=db, User=User, Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
 @app.route('/', methods=['GET','POST'])
 def index():
 	form = NameForm()
 	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user = User(username = form.name.data)
+			db.session.add(user)
+			session['known'] = False
+		else:
+			session['known'] = True
 		session['name'] = form.name.data
-		return redirect(url_for('index'))
 		form.name.data = ''
 
+		#old_name = session.get('name')
+		#if old_name is not None and old_name != form.name.data:
+		#	flash('Looks loke you changed your name!')
+		#session['name'] = form.name.data
+		#form.name.data=''
+		return redirect(url_for('index'))
 	return render_template('index.html', current_time=datetime.utcnow(),
-							form=form, name=session.get('name'))
+							form=form, name=session.get('name'),
+							known = session.get('known',False))
 
 @app.route('/user/<name>')
 def user(name):
